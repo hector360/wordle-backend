78a20d420dc6c9f638e1ca0c608c142cc6f6e848
hector sierra <hector360_m@hotmail.com>
2023-03-04 19:21:58 -0500

    removing comments

--- index.js
+++ index.js
@@ -13,28 +13,3 @@ function selectRandomWord() {
 console.log(selectRandomWord());
 
 
-//this goes in the web page:
-
-// const timerDuration = 5 * 60 * 1000; // 5 minutes in milliseconds
-// let timerExpirationTime = localStorage.getItem('timerExpirationTime');
-
-// if (!timerExpirationTime || Date.now() > timerExpirationTime) {
-//   // The timer has not been set or has expired, so set a new timer
-//   timerExpirationTime = Date.now() + timerDuration;
-//   localStorage.setItem('timerExpirationTime', timerExpirationTime);
-// }
-
-// // Start the timer countdown
-// const countdownInterval = setInterval(() => {
-//   const remainingTime = timerExpirationTime - Date.now();
-
-//   if (remainingTime <= 0) {
-//     // The timer has expired, so clear the interval and do something
-//     clearInterval(countdownInterval);
-//     localStorage.removeItem('timerExpirationTime');
-//     console.log('Timer expired!');
-//   } else {
-//     // Update the remaining time
-//     console.log(`Timer: ${remainingTime / 1000} seconds remaining`);
-//   }
-// }, 1000);
